8941054a74979a918b8f939113a98944ff6cad3d
the-dev-d <dhaivathl@gmail.com>
2022-03-25 13:09:09 +0530

    OS List added bug fixes

--- src/components/Collapsebar.jsx
+++ src/components/Collapsebar.jsx
@@ -8,21 +8,22 @@ class Collapsebar extends Component {
         }
     }
     render() { 
-        return (
-            <div className="card grid" style={{width:"100%",height:"auto",padding:"0px",marginBottom:"0.5rem",transition:"all 0.3s ease-in-out"}}>
-                        <button style={{width:"100%",margin:"0px"}} onClick={()=>this.setState((prev)=>{return{expand:!prev.expand}})}>
-                        <h3 className="flex" style={{padding:"0.5rem 0rem 0.5rem 0rem",width:"100%",justifyContent:"space-between",margin:"0px"}}>
+        return (    
+            <div className="expandcard card grid transition" style={{width:"100%",height:"fit-content",border: "1px solid var(--border)"}}>
+                        <button style={{width:"100%",margin:"0px",height:"fit-content"}} onClick={()=>this.setState((prev)=>{return{expand:!prev.expand}})}>
+                        <h3 className="flex" style={{padding:"0.5rem 0rem 0.5rem 0rem",width:"100%",height:"2rem",justifyContent:"space-between",margin:"0px"}}>
                             <div>
                                 &nbsp; {this.props.name} &nbsp; <i className={this.props.icon}></i>
                             </div>
                             <i style={{marginRight:"1rem"}} class="fa-solid fa-angle-down"></i> 
                             </h3>
                         </button>
-                <div className="transition flex flex-center" style={this.state.expand?{width:"100%",overflow:"hidden",height:"100%"}:{width:"100%",overflow:"hidden",height:"0rem"}}>
+                {this.state.expand&&<div className="transition flex flex-center">
                     <p style={{width:"90%",padding:"1rem"}}>
                         {this.props.children}
                     </p>
                 </div>
+                }
             </div>
         );
     }
