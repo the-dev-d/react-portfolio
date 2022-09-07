0f4904b74482ec9ad1684e2e3229625e55d4d36c
DEVD <devd@DEVD.DEVD>
2022-09-07 14:59:25 +0530

    Focusablility auto

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -1,21 +1,21 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './face.css';
 
 class Face extends Component {
     constructor(props) {
         super(props);
-        document.addEventListener("dbclick",()=>{
-            document.documentElement.requestFullscreen().catch((err)=>{
+        document.addEventListener("dbclick", () => {
+            document.documentElement.requestFullscreen().catch((err) => {
                 console.log(err);
             })
         })
     }
-    render() { 
+    render() {
         return (
             <>
-                <nav id="home" className='flex flex-center' style={{justifyContent:'space-between'}}> 
-                    <h3> DEV-D </h3> 
-                    <button onClick={()=>alert("Light Mode Colors are not decided yet, Please wait for some days")} className="fa-solid fa-sun darkmode"> </button>
+                <nav id="home" className='flex flex-center' style={{ justifyContent: 'space-between' }}>
+                    <h3> DEV-D </h3>
+                    <button onClick={() => alert("Light Mode Colors are not decided yet, Please wait for some days")} className="fa-solid fa-sun darkmode"> </button>
                 </nav>
                 <header className='flex-grid flex-grid-center'>
                     <div className="grid-flex-center social grid-flex">
@@ -25,8 +25,8 @@ class Face extends Component {
                         <a target="_blank" rel="noreferrer" href="https://github.com/the-dev-d/"> <i className="icon fa-brands fa-github"></i></a>
                     </div>
                     <div className="mainface flex flex-center" >
-                        <div className="face-container">
-                            <img className='mypic' src={this.props.image.src} alt="My Pic" />
+                        <div id="face-container" className="face-container">
+                            <img id="mypic" className='mypic' src={this.props.image.src} alt="My Pic" />
                             <div className="flicker-tile"></div>
                         </div>
                     </div>
@@ -35,6 +35,25 @@ class Face extends Component {
             </>
         );
     }
+
+    componentDidMount() {
+        const container = document.getElementById("face-container");
+        const pic = document.getElementById("mypic");
+
+        document.addEventListener('scroll',
+            () => {
+                const rect = container.getBoundingClientRect();
+                const visible = (
+                    rect.top >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.right <= (window.innerWidth || document.documentElement.clientWidth))
+
+                const { classList } = pic;
+                visible ? !classList.contains("focus") && pic.classList.add("focus") : classList.contains("focus") && pic.classList.remove("focus")
+            })
+    }
+
+
 }
- 
+
 export default Face;
